bbf9de3246723eda62fe1140475993e62aa6f0e4
rubiyadav18 <rubiyadav20@navgurukul.org>
2022-06-30 21:35:17 +0530

    task

---
+++ server.js
@@ -0,0 +1,20 @@
+// const { app,db, PORT } = require('./config/config');
+const express=require("express")
+// const { SERVER_DB_URI } = require('./constants/constants');
+const mongoose = require("mongoose")
+app.use(express.json())
+
+const db="mongodb+srv://moni07:moni07@firstproject.jvubt.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-bqqgzl-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+const bootstrap = async () => {
+  try {
+    await db.connect();
+    app.listen(PORT, async () => {
+
+      console.log(`server started ON ${PORT}`)
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+bootstrap();
